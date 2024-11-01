a77a5cb0e39dbc638479bc5339e480c77de6b6d2
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-01 19:07:12 -0400

    third

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var connect = require('connect');
+var http = require('http');
  
 var app = connect();
- function 
+ function Aboutme(req,res,next)
+{
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hi, My name is sharubaa');
 
-// respond to all requests
-app.use(function(req, res){
-  res.end('Hello\n');
-});
- 
+}
+
+function Projects(req,res,next)
+{
+    res.setHeader('Content-Type','text/plain');
+    res.end('These are my projects');
+
+}
+
 //create node.js http server and listen on port
+app.use('/Aboutme',Aboutme)
+app.use('/Projects',Projects)
 http.createServer(app).listen(3000);
+console.log('Server is running')

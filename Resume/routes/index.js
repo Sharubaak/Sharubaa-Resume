74db1b247d1578bac54384f629ed8ac756e46f5f
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-01 22:51:42 -0400

    Base of website done

--- Resume/routes/index.js
+++ Resume/routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('index', { 
     title: 'My Resume' });
 });
 
 /* GET about me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: 'About Me' });
 });
 
 /* GET video of me page. */

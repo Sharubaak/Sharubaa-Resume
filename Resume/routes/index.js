fd7a5293e9aa68f0d313b74140df81d05caff844
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-03 23:56:36 -0500

    done after this

--- Resume/routes/index.js
+++ Resume/routes/index.js
@@ -1,29 +1,30 @@
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { 
+  res.render('home', { 
     title: 'Resume' });
 });
 
 /* GET about me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET video of me page. */
 router.get('/video', function(req, res, next) {
-  res.render('index', { title: 'video about me' });
+  res.render('video', { title: 'video about me' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contact', { title: 'Contact Me' });
 });
 module.exports = router;

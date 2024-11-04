000a1aa0321154bbade15a52ea52395c7f849198
Sharubaa <146009109+Sharubaa@users.noreply.github.com>
2024-11-04 00:31:58 -0500

    fix?

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the dynamic port provided by Heroku
 
 app.get('/AboutMe', (req, res) => {
-  res.send('Hello!')
-})
+  res.send('Hello!');
+});
 
 app.get('/Projects', (req, res) => {
-    res.send('These are a couple of projects I like to show')
-  })
-  app.get('/Contactus', (req, res) => {
-    res.send('If you have any questions do not hestitate. Send me a message down below or contact me on my email')
-  })
+  res.send('These are a couple of projects I like to show');
+});
 
-  app.get('/AboutMe', (req, res) => {
-    res.send('Hello!')
-  })
+app.get('/Contactus', (req, res) => {
+  res.send('If you have any questions, do not hesitate to send me a message or contact me via email');
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
